a8bb58d693714e5a03f39438d5bdf1401793bade
chamulong <393829394@qq.com>
2018-04-20 08:54:58 +0800

    增加公共js文件，异步加载第三方js库

---
+++ src/main/webapp/common.js
@@ -0,0 +1,3 @@
+head.js("https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js");
+head.js("https://cdn.bootcss.com/bootstrap/4.1.0/js/bootstrap.min.js");
+head.js("https://cdn.bootcss.com/bootstrap/4.1.0/css/bootstrap.min.css");
