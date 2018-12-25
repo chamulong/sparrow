71a30df674f40fcc8c5dc57dc1fc9ae60d739485
393829394@qq.com <xtlh_jcj_1102>
2018-12-25 09:42:36 +0800

    完善0x04协议，新增0x05协议

--- src/main/java/com/jcj/sparrow/NettyServerService/protocol/Radar_04.java
+++ src/main/java/com/jcj/sparrow/NettyServerService/protocol/Radar_04.java
@@ -1,5 +1,9 @@
 package com.jcj.sparrow.NettyServerService.protocol;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.Unpooled;
+
 /**
  * @Author: 江成军
  * @Date: 2018/12/24 19:55
@@ -8,15 +12,53 @@ package com.jcj.sparrow.NettyServerService.protocol;
  */
 public class Radar_04 implements IMessageBody
 {
+
+    // 目标数量（单位：个）
+    public int TargetNum;
+
+    // 目标距离(单位：米)
+    public float[] TargetDistance;
+
+    public Radar_04()
+    {
+        TargetNum = 0;
+    }
+
     @Override
     public byte[] WriteToBytes()
     {
-        return new byte[0];
+        ByteBuf buff= ByteBufAllocator.DEFAULT.ioBuffer();
+        buff.writeShort(0);
+        return buff.array();
     }
 
     @Override
     public void ReadFromBytes(byte[] messageBodyBytes)
     {
+        ByteBuf buff = Unpooled.copiedBuffer(messageBodyBytes);
+        TargetNum = (int)buff.readFloat();
+        TargetDistance = new float[TargetNum];
+        for (int i = 0; i < TargetNum; i++)
+        {
+            TargetDistance[i] = buff.readFloat();
+        }
+        int otherdata = (99 - TargetNum) * 4;
+        buff.readBytes(otherdata);
+    }
 
+    public int getTargetNum() {
+        return TargetNum;
+    }
+
+    public void setTargetNum(int targetNum) {
+        TargetNum = targetNum;
+    }
+
+    public float[] getTargetDistance() {
+        return TargetDistance;
+    }
+
+    public void setTargetDistance(float[] targetDistance) {
+        TargetDistance = targetDistance;
     }
 }
